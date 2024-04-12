15888307ca7e8674001e0a7efeee88379c57b7cc
EMPEROR <sahilsorte8457@gmail.com>
2024-04-12 21:24:59 +0530

    Authorization based on org id and tokenIdentifier

--- convex/files.ts
+++ convex/files.ts
@@ -1,26 +1,50 @@
 import { ConvexError, v } from "convex/values";
-import { mutation, query } from "../convex/_generated/server";
+import { MutationCtx, QueryCtx, mutation, query } from "../convex/_generated/server";
+import { getUser } from "./users";
+
+async function hasAccessToOrg(ctx: QueryCtx | MutationCtx, tokenIdentifier: string, orgId: string) {
+    const user = await getUser(ctx, tokenIdentifier);
+
+    const hasAccess = user.orgIds.includes(orgId) || user.tokenIdentifier.includes(orgId);
+
+    return hasAccess;
+}
 
 export const createFile = mutation({
     args: {
         name: v.string(),
+        orgId: v.string(),
     },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
-        if(!identity){
+        // console.log(identity);
+        if (!identity) {
             throw new ConvexError("You must be logged in to upload a file");
         }
-        await ctx.db.insert("files", { name: args.name });
+        const hasAccess = await hasAccessToOrg(ctx, identity.tokenIdentifier, args.orgId);
+        if (!hasAccess) {
+            throw new ConvexError("You must be a member of the organization to upload a file");
+        }
+
+        await ctx.db.insert("files", { name: args.name, orgId: args.orgId });
     },
 });
 
 export const getFiles = query({
-    args: {},
+    args: {
+        orgId: v.string(),
+    },
     async handler(ctx, args) {
         const identity = await ctx.auth.getUserIdentity();
-        if(!identity){
+        if (!identity) {
             return [];
         }
-        return await ctx.db.query('files').collect();
+        const hasAccess = await hasAccessToOrg(ctx, identity.tokenIdentifier, args.orgId);
+        if (!hasAccess) {
+            return [];
+        }
+        return await ctx.db.query('files').withIndex('by_orgId', q =>
+            q.eq('orgId', args.orgId)
+        ).collect();
     },
 });
---
+++ src/app/header.tsx
@@ -0,0 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { OrganizationSwitcher, SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+
+export function Header() {
+    return <div className="border-b py-4 bg-gray-50">
+        <div className="items-center container mx-auto justify-between flex">
+            <div>CloudHorizon</div>
+            <div className="flex gap-2">
+                <OrganizationSwitcher />
+                <UserButton />
+                <SignedOut>
+                <SignInButton>
+                    <Button>Sign In</Button>
+                </SignInButton>
+                </SignedOut>
+            </div>
+        </div>
+    </div>
+}
