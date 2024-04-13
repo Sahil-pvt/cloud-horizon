38780d3e724315ac73fedff1a5d1494f42ec0fc6
EMPEROR <sahilsorte8457@gmail.com>
2024-04-14 02:09:12 +0530

    Minor header update

--- src/app/header.tsx
+++ src/app/header.tsx
@@ -1,17 +1,23 @@
 import { Button } from "@/components/ui/button";
 import { OrganizationSwitcher, SignInButton, SignedOut, UserButton } from "@clerk/nextjs";
+import Image from "next/image";
+import Link from "next/link";
 
 export function Header() {
     return <div className="border-b py-4 bg-gray-50">
         <div className="items-center container mx-auto justify-between flex">
-            <div>CloudHorizon</div>
+            <Link href="/" className="flex gap-2 items-center text-xl">
+                {/* <Image src="/logo.png" width="50" height="50" alt="logo" /> */}
+                CloudHorizon
+            </Link>
+            <Button variant={"outline"}><Link href="/dashboard/files">Your Files</Link></Button>
             <div className="flex gap-2">
                 <OrganizationSwitcher />
                 <UserButton />
                 <SignedOut>
-                <SignInButton>
-                    <Button>Sign In</Button>
-                </SignInButton>
+                    <SignInButton>
+                        <Button>Sign In</Button>
+                    </SignInButton>
                 </SignedOut>
             </div>
         </div>
