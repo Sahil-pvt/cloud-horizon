a2e823f458a7515d77d27c08b4e0651141574ae7
EMPEROR <sahilsorte8457@gmail.com>
2024-04-25 21:18:24 +0530

    Favicon update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,6 +11,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   title: "Cloud Horizon",
   description: "File Drive",
+  // icons: {
+  //   icon: "favicon.ico",
+  // }
 };
 
 export default function RootLayout({
